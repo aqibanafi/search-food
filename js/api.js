5056164ac0e98d0e428afb0df0f117c87a632b0e
unknown <you@example.com>
2022-08-30 16:51:14 +0600

    Extra Added

--- js/api.js
+++ js/api.js
@@ -13,6 +13,12 @@ const displaySearchFood = foodItem => {
     spinner.classList.add('hidden')
     const getMainDiv = document.getElementById('main-div');
     getMainDiv.innerHTML = '';
+    if(foodItem === null) {
+        const warning = document.getElementById('warning');
+        warning.classList.remove('hidden')
+    } else {
+        warning.classList.add('hidden')
+    }
     foodItem.forEach(itemsName => {
         const createDiv = document.createElement('div');
         createDiv.innerHTML = `
@@ -29,18 +35,18 @@ const displaySearchFood = foodItem => {
             </div>
         </div>
             `;
-        getMainDiv.appendChild(createDiv)
+        getMainDiv.appendChild(createDiv);       
 })
 }
-
 const getAllFood = allFood => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
     .then(res => res.json())
-    .then(display => displayAllFood(display.meals))
+    .then(display => displayAllFood(display.meals.slice(0,10)))
 }
 const displayAllFood = displayFood => {
     const getMainDiv = document.getElementById('main-div');
     displayFood.forEach(items => {
+        
         const createDiv = document.createElement('div');
         createDiv.innerHTML = `
         <div class="flex justify-center mt-20">
@@ -56,11 +62,48 @@ const displayAllFood = displayFood => {
             </div>
         </div>
             `;
+            
         getMainDiv.appendChild(createDiv)
     })
 }
 getAllFood();
-
+document.getElementById('show-all-btn').addEventListener('click', function(){
+    const getAllFood = allFood => {
+        fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
+        .then(res => res.json())
+        .then(display => displayAllFood(display.meals))
+    }
+    let count = 0;
+    count++
+    if(count === 1){
+        const getButton = document.getElementById('show-all-btn');
+        getButton.classList.add('hidden');
+    }
+    const displayAllFood = displayFood => {
+        const getMainDiv = document.getElementById('main-div');
+        displayFood.forEach(items => {
+            
+            const createDiv = document.createElement('div');
+            createDiv.innerHTML = `
+            <div class="flex justify-center mt-20">
+                <div class="rounded-lg shadow-lg bg-white max-w-sm">
+                <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
+                    <img class="rounded-t-lg" src="${items.strMealThumb}" alt=""/>
+                </a>
+                <div class="p-6">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">${items.strMeal}</h5>
+                    <p>${items.strInstructions.slice(0,100)}</p>
+                    <button onclick="foodById(${items.idMeal})" type="button" class="mt-12 inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-400 hover:shadow-lg focus:bg-red-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-lg transition duration-150 ease-in-out" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> Show Detail </button>
+                </div>
+                </div>
+            </div>
+                `;
+                
+            getMainDiv.appendChild(createDiv)
+        })
+    }
+    getAllFood();
+})
 const foodById = searchId => {
     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${searchId}`
     fetch(url)
@@ -82,3 +125,10 @@ const openModal = modal => {
     getModal.innerHTML = '';
     getModal.appendChild(createModalBody);
 }
+const getSearchField = document.getElementById('search-field');
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    // console.log(event.target.value)
+    if (e.key === 'Enter') {
+        searchFood();
+    }
+})
